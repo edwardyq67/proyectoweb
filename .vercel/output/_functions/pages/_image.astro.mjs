c6cfcde4db79ad71be971085459404475992121c
edward672001 <edwardyq200167@gmail.com>
2025-12-29 18:41:28 -0500

    rrrr

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_CgCzPm0g.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
-import vercel from '@astrojs/vercel'; // Necesitas adapter
-
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node'; // Agregar esto
 
 export default defineConfig({
   integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'static', // Cambiar a server
-  adapter: netlify(), // Necesitas adapter para SSR
-  site: 'https://tudominio.com',
+  output: 'static',
+  adapter: node({
+    mode: 'standalone' // Para static generation
+  })
 });
